7140f5d687b244d8ccdf76aa4ca0727d34fc6d89
sretundijr <sretundijr@gmail.com>
2017-04-08 14:01:43 -0600

    Built temp question and answer data structure

---
+++ questions.js
@@ -0,0 +1,23 @@
+var quizQuestions = {
+    questions: [
+        [
+            'After performing a cylinder pressure test on a four cylinder engine, the #1 cylinder is 30 psi below specifications. '+
+            'All the other cylinders have passed. The pressure in the #1 cylinder increased after injecting engine oil. '+
+            'Technician A says excessive carbon buildup on the piston head has caused these test results. '+
+            'Technician B says these results indicate a faulty intake or exhaust valve. Who is correct?', 
+            'Technician A',
+            'Technician B', 
+            'Both Technician A and B',
+            'Neither Technician A or B'
+        ],
+        [
+            'A vehicle with a vacuum brake booster has a hard pedal with little stopping power.'+
+            'Technician A says a kink in the check valve\'s hose will result in this condition. '+
+            'Technician B says a vacuum booster needs at least 12 Hg of vacuum to operate properly. Who is correct?',
+            'Technician A',
+            'Technician B',
+            'Both Technician A and B',
+            'Neither Technician A or B'
+        ]
+    ]
+}
